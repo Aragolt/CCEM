a062717da655ff4e265c94fbcd1c42d1f3943ca4
lobbienjonsji <robin.sigmund@web.de>
2019-12-09 08:58:22 +0100

    Renamed Razor to Memento Mori

--- src/main/java/theextravagant/cards/Razor.java
+++ src/main/java/theextravagant/cards/Razor.java
@@ -18,7 +18,7 @@ public class Razor extends CustomCard {
 
 
     public static final String ID = theextravagant.makeID("Razor");
-    public static final String IMG = theextravagant.makeCardPath("Razor.png");
+    public static final String IMG = theextravagant.makeCardPath("Razornew.png");
     public static final CardColor COLOR = TheExtravagant.Enums.EV_BLUE;
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
